afc578ae615124704e191dc6bb6b006bf5abbdc5
Ihor Chernomorets <oxnardxvi@gmail.com>
2023-11-28 19:56:23 +0100

    Tasks completed

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int digit1 = number / 1000;
+        int digit2 = number / 100 - digit1 * 10;
+        int digit3 = number / 10 - (digit1 * 100 + digit2 * 10);
+        int digit4 = number % 10;
+
+        int sum = digit1 + digit2 + digit3 + digit4;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int digit1 = number / 100;
+        int digit2 = number / 10 - digit1 * 10;
+        int digit3 = number % 10;
+
+        int reversed = digit3 * 100 + digit2 * 10 + digit1;
+        System.out.println(reversed);
     }
 }
